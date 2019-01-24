c53425c1b9498ed0b708c5921210f6ccabf7de25
Melanie Ryan <melaniehoperyan@gmail.com>
2019-01-23 20:37:40 -0800

    cards are shuffling

--- src/App.js
+++ src/App.js
@@ -14,21 +14,20 @@ class App extends Component {
   };
 
 
-  // componentDidMount() {
-  //   const newDoggies = shuffleArray(this.props.doggies)
-  //   this.setNewRandomId(this.state.doggies);
-  // }
+  componentDidMount() {
+    const newDoggies = this.shuffleArray(cards);
+    this.setState({doggies: newDoggies});
+  }
 
-  // shuffleArray(array) {
-  //   let i = array.length - 1;
-  //   for (; i > 0; i--) {
-  //     const j = Math.floor(Math.random() * (i + 1));
-  //     const temp = array[i];
-  //     array[i] = array[j];
-  //     array[j] = temp;
-  //   }
-  //   return array;
-  // }
+  shuffleArray(array) {
+    for ( let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
 
 
   render() {
