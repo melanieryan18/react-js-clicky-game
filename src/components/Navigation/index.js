93fb7643b4ff069b2df50e36fd8bf1a421f2b834
Melanie Ryan <melaniehoperyan@gmail.com>
2019-01-25 15:20:59 -0800

    ready to deploy

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 
 function Navigation(props) {
     return (
-    <nav className="navbar">
-        <ul>
-            <li className="brand">
-            <a href="/">Clicky Game</a>
-            </li>
-            <li className="">{props.result}</li>
-            <li className="">Score: {props.score} | top Score: {props.topScore} </li>
-        </ul>
-    </nav>
-)
+        <div>
+            <ul class="nav row">
+                <li className="nav-item col-3">
+                    <a href="/" className="nav-link">Clicky Game</a>
+                </li>
+                <li className="nav-item col-3">
+                    <a href="/" className="nav-link">{props.result}</a>
+                </li>
+                <li className="nav-item col-3">
+                    <a href="/" className="nav-link">Score: {props.score} | Top Score: {props.topScore}</a>
+                </li>
+            </ul>
+            <div className="jumbotron">
+                <h1 className="center display-4">Welcome to Puppy Clicker!</h1>
+                <h5 className="center">Click the puppy pictures to pet the dogs. Don't pet a dog more than once, or you will loose a point. </h5>
+            </div>
+        </div>
+    )
 }
 
 export default Navigation;
